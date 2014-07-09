c1efcb2baaed7a9c6c8613348b6bb7889a4a543e
3rd-Eden <info@3rd-Eden.com>
2014-07-09 11:35:53 +0200

    [fix] Introduce assume as global library

--- browserify.js
+++ browserify.js
@@ -19,18 +19,25 @@ module.exports = function compile(files, options, fn) {
     options = {};
   }
 
-  var b = browserify(options);
-
   //
   // Introduce our `assume` library by default in to the package so you only
   // need Illuminati to start testing your applications and add all other files
   // that are needed to test all the things.
   //
-  b.require('assume');
+  options.builtins = require('browserify/lib/builtins.js');
+  options.builtins.assume = require.resolve('assume');
+
+  var b = browserify(options);
+
+  //
+  // Add all the test files as entry files so they will be executed when the
+  // browserify bundle is loaded. If they are not executed, we will not be able
+  // to run the test suite.
+  //
   files.forEach(b.add.bind(b));
 
   b.bundle({
-    debug: true       // Ensure that browserify is compiled with source-maps.
+    debug: true           // Ensure that browserify is compiled with source-maps.
   }, function bundled(err, source) {
     if (err) return fn(err);
 
